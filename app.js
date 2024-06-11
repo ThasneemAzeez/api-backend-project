e51cb5fe0e2566d6dfb2ccf985b14fb9c5ed34fe
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-11 14:08:26 +0530

    completed

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-const student=require("./models/student")
+const {studentmodel} =require("./models/student")
 
 const app = express()
 app.use(cors())
+app.use(express.json())
+
+//mongodb connection
+mongoose.connect("mongodb+srv://thasneemazeez:thasneem38@cluster0.uk9okno.mongodb.net/studentdb?retryWrites=true&w=majority&appName=Cluster0")
 
 app.get("/",(req,res)=>{
     res.send("hello")
@@ -14,10 +18,27 @@ app.post("/contact",(req,res)=>{
     res.send("welcome to my contact page")
 })
 
-app.listen(8080,()=>{
-    console.log("server started")
-})
 
 app.post("/add",(req,res)=>{
-    res.send("")
+    let input=req.body //read input
+    let student=new studentmodel(input) //passing to model
+    student.save()
+    res.json({"status":"success"}) // checking input
+})
+
+app.get("/view",(req,res)=>{
+    studentmodel.find().then(
+        (data)=>{
+res.json(data)
+        }
+    ).catch((error)=>{
+res.json(error)
+    })
+})
+
+
+
+
+app.listen(8080,()=>{
+    console.log("server started")
 })
--- models/student.js
+++ models/student.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose")
 const scheema = mongoose.Schema(
     {
-        "name":String,
-        "rollno":String,
-        "admnNo":String,
-        "college":String
+        "name":{type:String,required:true},
+        "rollno":{type:String,required:true},
+        "admnNo":{type:String,required:true},
+        "college":{type:String,required:true}
     }
 )
 
 let studentmodel= mongoose.model("students",scheema);
+module.exports={studentmodel}
